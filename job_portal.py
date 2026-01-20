44c6dd2eef5b6dd8d1c6588deca85a8ef3d5182c
afouziya2004 <afouziya2004@gmail.com>
2026-01-20 18:39:13 +0530

    Day 3: Added interactive menu and user input for jobs

--- job_portal.py
+++ job_portal.py
@@ -1,25 +1,41 @@
-# Simple Job Portal (Day 2)
+# Simple Job Portal (Day 3)
 
 jobs = []
 
-def add_job(title, company):
+def add_job():
+    title = input("Enter job title: ")
+    company = input("Enter company name: ")
     job = {
         "title": title,
         "company": company
     }
     jobs.append(job)
-    print("Job added successfully")
+    print("Job added successfully!\n")
 
 def list_jobs():
     if not jobs:
-        print("No jobs available")
+        print("No jobs available\n")
     else:
+        print("\nAvailable Jobs:")
         for index, job in enumerate(jobs, start=1):
             print(f"{index}. {job['title']} at {job['company']}")
+        print()
 
-# Sample usage
-add_job("Python Developer", "Startup Company")
-add_job("Backend Engineer", "Product Company")
+def menu():
+    while True:
+        print("1. Add Job")
+        print("2. List Jobs")
+        print("3. Exit")
+        choice = input("Choose an option: ")
 
-print("\nAvailable Jobs:")
-list_jobs()
+        if choice == "1":
+            add_job()
+        elif choice == "2":
+            list_jobs()
+        elif choice == "3":
+            print("Exiting Job Portal. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Try again.\n")
+
+menu()
